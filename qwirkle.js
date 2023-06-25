68948fcc6372fef9727d23b70950ae6df859faac
Jasper Eenhoorn <een@emmauscollege.nl>
2023-06-25 22:23:34 +0200

    extract hand class

--- qwirkle.js
+++ qwirkle.js
@@ -88,38 +88,7 @@ window.addEventListener('mousedown', () => {
     console.log(cursor.last);
 });
 
-// import { hand } from "./hand.js";
-class hand {
-    constructor(cnv, bag, pos = { x: 25, y: 25 }) {
-        this.cnv = cnv;
-        this.ctx = ctx;
-        this.cnv.entities.push(this);
-
-        this.pos = pos;
-        this.bag = bag;
-        this.tiles = [];
-        for(let i=0; i<6; i++) {
-            this.tiles.push(bag.pop());
-        }
-        this.update();
-    }
-    move(pos) {
-        this.pos = pos;
-        this.update();
-        this.draw();
-    }
-    update() {
-        for(let i=0; i<6; i++) {
-            this.tiles[i].move({
-                x: this.pos.x + i*this.tiles[0].size,
-                y: this.pos.y
-            })
-        }
-    }
-    draw() {
-        this.tiles.map(t => t.draw());
-    }
-}
+import { hand } from "./hand.js";
 const hands = [];
 hands.positions = [
     { x: 50, y: cnv.height - 75 },
