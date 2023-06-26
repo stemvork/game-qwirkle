84a14add0acaa1d711b51b7f9b3b9381c524f896
Jasper Eenhoorn <een@emmauscollege.nl>
2023-06-26 09:39:53 +0200

    finished refactoring

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ import { hands } from "./hand.js";
 import { cursor } from "./cursor.js";
 import { board } from "./board.js";
 import { scoreboard } from "./scoreboard.js";
+import { square } from "./square.js";
+import { last } from "./utils.js";
 export class game {
     constructor(cnv) {
         this.cnv = cnv;
@@ -17,4 +19,42 @@ export class game {
     }
     update() {}
     draw() {}
+    nextPlayer() { this.hands.next(); }
+    selectTile() {
+        this.cursor.from(this.hands.hands[this.hands.current].tiles[0]);
+        this.hands.selectedTile = 0;
+    }
+    keyDown(e) {
+        if(e.key === ' ') this.nextPlayer();
+        else if(e.key === '1') this.selectTile(0);
+        else if(e.key === '2') this.selectTile(1);
+        else if(e.key === '3') this.selectTile(2);
+        else if(e.key === '4') this.selectTile(3);
+        else if(e.key === '5') this.selectTile(4);
+        else if(e.key === '6') this.selectTile(5);
+    }
+    moveMouse(e) {
+        const [ci, cj] = [
+            Math.round(e.offsetX/square.default_size),
+            Math.round(e.offsetY/square.default_size),
+        ];
+        this.cursor.square.move({
+            x: ci*square.default_size,
+            y: cj*square.default_size
+        });
+        this.cursor.last = { i: ci, j: cj };
+    }
+    clickMouse(e) {
+        if(this.hands.selectedTile === null) return;
+        if(!this.board.isConnected()) return;
+        const new_pos = this.cursor.get_pos(this.cursor.last);
+        this.board.tiles.push(this.cursor.square.copy(new_pos));
+        last(this.board.tiles).index = this.cursor.last;
+        const new_tile = this.bag.tiles.pop();
+        // BUG: would be easier to "change" tiles, instead of creating new
+        new_tile.hidden = true;
+        const currentHand = this.hands.hands[this.hands.current];
+        currentHand.tiles[this.hands.selectedTile] = new_tile;
+        this.cursor.from(new_tile);
+    }
 }
--- qwirkle.js
+++ qwirkle.js
@@ -32,54 +32,7 @@ window.requestAnimationFrame(draw);
 // Game specific logic
 import { game } from "./game.js";
 const g = new game(cnv);
-window.addEventListener('keydown', e => {
-    if(e.key === ' ') {
-        g.hands.next();
-    } else if(e.key === '1') {
-        g.cursor.from(g.hands.hands[g.hands.current].tiles[0]);
-        g.hands.selectedTile = 0;
-    } else if(e.key === '2') {
-        g.cursor.from(g.hands.hands[g.hands.current].tiles[1]);
-        g.hands.selectedTile = 1;
-    } else if(e.key === '3') {
-        g.cursor.from(g.hands.hands[g.hands.current].tiles[2]);
-        g.hands.selectedTile = 2;
-    } else if(e.key === '4') {
-        g.cursor.from(g.hands.hands[g.hands.current].tiles[3]);
-        g.hands.selectedTile = 3;
-    } else if(e.key === '5') {
-        g.cursor.from(g.hands.hands[g.hands.current].tiles[4]);
-        g.hands.selectedTile = 4;
-    } else if(e.key === '6') {
-        g.cursor.from(g.hands.hands[g.hands.current].tiles[5]);
-        g.hands.selectedTile = 5;
-    }
-});
 
-
-import { square } from "./square.js";
-window.addEventListener('mousemove', e => {
-    const [ci, cj] = [
-        Math.round(e.offsetX/square.default_size),
-        Math.round(e.offsetY/square.default_size),
-    ];
-    g.cursor.square.move({
-        x: ci*square.default_size,
-        y: cj*square.default_size
-    });
-    g.cursor.last = { i: ci, j: cj };
-});
-import { last } from "./utils.js";
-window.addEventListener('mousedown', () => {
-    if(g.hands.selectedTile === null) return;
-    if(!g.board.isConnected()) return;
-    const new_pos = g.cursor.get_pos(g.cursor.last);
-    g.board.tiles.push(g.cursor.square.copy(new_pos));
-    last(g.board.tiles).index = g.cursor.last;
-    const new_tile = g.bag.tiles.pop();
-    // BUG: would be easier to "change" tiles, instead of creating new
-    new_tile.hidden = true;
-    g.hands.hands[g.hands.current].tiles[g.hands.selectedTile] = new_tile;
-    g.cursor.from(new_tile);
-});
-
+window.addEventListener('keydown', e => g.keyDown(e));
+window.addEventListener('mousemove', e => g.moveMouse(e));
+window.addEventListener('mousedown', e => g.clickMouse(e));
